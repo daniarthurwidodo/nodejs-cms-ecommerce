f1e52fe9a5c3bb256ca890009cb314e851ca720d
Dani Arthur Widodo <work.daniarthurwidodo@gmail.com>
2023-10-01 14:59:19 +0700

    setup database

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,28 @@
 import express, { Application, Request, Response } from 'express'
+import { DataSource } from 'typeorm'
+const AppDataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "P@ssword1",
+    database: "node_ecommerce",
 
+})
 const app: Application = express()
 
 const port: number = 3001
 
-app.get('/toto', (req: Request, res: Response) => {
-    res.send('Hello toto')
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err)
+    })
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hi mom!')
 })
 
 app.listen(port, function () {
